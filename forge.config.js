fa13787868bcf25e65fdbcf1990b48637b181c0c
Joey ter Meulen <joeytermeulen@gmail.com>
2024-05-22 16:29:52 +0200

    chore: publishers

--- forge.config.js
+++ forge.config.js
@@ -41,4 +41,15 @@ module.exports = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "Joey ter Meulen",
+          name: "electron-photo-sorter"
+        }
+      }
+    }
+  ]
 };
